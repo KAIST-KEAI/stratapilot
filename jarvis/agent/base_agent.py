0cc0b53d00080b04b0eda11169dfabe5b2c14fd3
hyun <148046327+song-hyun-seob@users.noreply.github.com>
2024-09-23 10:24:00 +0800

    add travel eval

--- jarvis/agent/base_agent.py
+++ jarvis/agent/base_agent.py
@@ -26,10 +26,14 @@ class BaseAgent:
         sys.path.append(path)
         files = glob.glob(path + "/*.py")
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e37198f (add travel eval)
         for file in files:
             if file.endswith('.py') and "__init__" not in file:
                 class_name = file[:-3].split('/')[-1]  # 去除.py后缀，获取类名
                 module = importlib.import_module(class_name)
+<<<<<<< HEAD
                 tmp_obj = getattr(module, class_name)()
                 self.action_lib.update({class_name: tmp_obj})
                 self.action_lib_description.update({class_name: tmp_obj.description})
@@ -43,6 +47,14 @@ class BaseAgent:
         #         self.action_lib_description.update({class_name: tmp_obj.description})
 >>>>>>> e3c3923 (dev SkillCreator)
 
+=======
+                source_code = inspect.getsource(module) # wzm修改，通过自省获得文件源码
+                tmp_obj = getattr(module, class_name)() #存储对象方式
+                # 存储源码字符串
+                self.action_lib.update({class_name:  source_code})# wzm修改，技能库存储文件源码而不是对象
+                # self.action_lib.update({class_name: tmp_obj})
+                self.action_lib_description.update({class_name: tmp_obj.description})
+>>>>>>> e37198f (add travel eval)
 
 if __name__ == '__main__':
     a = BaseAgent()
